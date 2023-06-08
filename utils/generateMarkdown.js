06a786a370ddac9606fc7cfe5502c0a03e5f54fb
Tawhid <tawhidkamal5@gmail.com>
2023-06-08 18:16:00 -0400

    thinggs be workiun

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license === 'MIT'){
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-  }else if (license === 'APACHE'){
+  if (license === 'APACHE'){
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  }else if(license === 'MIT'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
   } else if (license === 'BSD'){
     return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
-  } else if (license ==='IBM'){
-    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+  } else if (license ==='GNU'){
+    return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
   } else {
     return '';
   }
@@ -17,18 +17,20 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (license === 'APACHE'){
+    return 'https://www.apache.org/licenses/LICENSE-2.0';
+  }
   if (license === 'MIT'){
     return '(https://opensource.org/licenses/MIT)';
-  }else if (license === 'APACHE'){
-    return 'https://www.apache.org/licenses/LICENSE-2.0';
-  }else if (license === 'BSD'){
+  }
+  if (license === 'BSD'){
     return 'https://opensource.org/license/bsd-3-clause/';
-  }else if (license === 'GNU'){
-    return '(https://www.gnu.org/licenses/gpl-3.0)';
-  }else {
+  }
+  if (license === 'GNU'){
+    return '(https://www.gnu.org/licenses/gpl-3.0)';}
+  if(license === 'None'){
     return '';
   }
-  
 }
 
 // TODO: Create a function that returns the license section of README
@@ -51,6 +53,9 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+
   ## Description
   ${data.description}
 
@@ -63,13 +68,14 @@ function generateMarkdown(data) {
   6. [Questions](#questions)
 
   ## Installation
-  User must install/run: ${data.installation}
+  User must insStall/run: ${data.installation}
 
   ## Usage
   ${data.usage}
 
   ## License 
-  ${data.license}
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
 
   ## Contributing
   ${data.contribution}
