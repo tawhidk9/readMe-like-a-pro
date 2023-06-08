bda0c7748643093e40176921687445b77f364831
Tawhid <tawhidkamal5@gmail.com>
2023-06-07 23:23:41 -0400

    added questions array, create writetofile func, create init func

---
+++ index.js
@@ -0,0 +1,64 @@
+//Dependencies
+const inquire = require('inquirer');
+const fs = require('fs');
+const markdown = require ('generateMarkdown.js');
+
+//Create questions
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the project Title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Provide a brief description of the project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Does the User have to install anything to use this application?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Give a description on how to use the application',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What does the User need to know about contributions/contributing to the repo?',
+        name: 'contribution'
+    },
+    {
+        type: 'list',
+        message: 'What license was used for the project?',
+        name: 'license',
+        choices: [
+            'APACHE',
+            'MIT',
+            'BSD',
+            'GPL',
+            'None'
+        ]
+    }
+];
+
+//Function to write readME file 
+function writeToFile(fileName, data){
+    let data = markdown(data);
+    fs.writeFile(fileName, info, (err) =>
+    (err) ? 'error': 'success')
+};
+
+//Function to initialize 
+function init () {
+    inquire
+    .prompt (questions)
+    .then((data) => {
+        const fileName = 'readMe.md';
+        writeToFile(fileName, data);
+    });
+};
+
+init ();
